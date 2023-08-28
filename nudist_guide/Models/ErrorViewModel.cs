331e87e4bd4211ca631a9a29e5d6b8912533ddcf
DaCoPro <comerbdavid@gmail.com>
2023-08-28 11:28:52 -0700

    init.

---
+++ nudist_guide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace nudist_guide.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
